914f878882a6987a80873d8c0272955663de17f4
RobinDevillers <re.devillers1999@gmail.com>
2020-01-14 16:47:03 +0100

    made a dictionary from the amount and the percentage per month

--- monthly_rain_seattle.py
+++ monthly_rain_seattle.py
@@ -12,6 +12,7 @@ with open ('precipitation.json', encoding= 'utf8') as file:
 #step 2: Write a code to categorise the dates per month
 #limit data to only the first 
 
+
 months_seattle = [0]*12
 seattle = 'GHCND:US1WAKG0038'
 
@@ -21,36 +22,21 @@ for observation in data:
             month = int(date[1]) -1
             months_seattle[month] += observation['value']
 
-months_cincinnati = [0]*12
-cincinnati = 'GHCND:USW00093814'
+print(months_seattle)
+total_rain = sum(months_seattle)
 
-for observation in data:
-    if observation['station'] == cincinnati:
-            date = observation['date'].split('-')
-            month = int(date[1]) -1
-            months_cincinnati[month] += observation['value']
 
-months_maui = [0]*12
-maui = 'GHCND:USC00513317'
+percentage_per_month = []
+for month in months_seattle:
+    percentage = (month/total_rain)*100
+    percentage_per_month.append(percentage)
+    print(percentage)
 
-for observation in data:
-    if observation['station'] == maui:
-            date = observation['date'].split('-')
-            month = int(date[1]) -1
-            months_maui[month] += observation['value']
+seattle_monthly_amount_and_percentage = {}
+seattle_monthly_amount_and_percentage['Amount per month'] = months_seattle
+seattle_monthly_amount_and_percentage['Percentage per month'] = percentage_per_month
 
-months_sandiego = [0]*12
-sandiego = 'GHCND:US1CASD0032'
+print(seattle_monthly_amount_and_percentage)
 
-for observation in data:
-    if observation['station'] == sandiego:
-            date = observation['date'].split('-')
-            month = int(date[1]) -1
-            months_sandiego[month] += observation['value']
-
-print(f'Seattle per month {months_seattle}')
-print(f'Cincinnati per month {months_cincinnati}')
-print(f'Maui per month {months_maui}')
-print(f'San Diego per month {months_sandiego}')
-print(f'In a year, the total amount of rain at these four stations is {sum(months_seattle) + sum(months_cincinnati) + sum(months_maui) + sum(months_sandiego)}')
-
+# with open('file_name.json', 'w' encoding=utf8) as file:
+#     json.dump(some_data, file)
