6da890c835c813078f00673f474efad8289ae478
RobinDevillers <re.devillers1999@gmail.com>
2020-01-14 14:41:01 +0100

    change 'date' to be displayed as month

--- monthly_rain_seattle.py
+++ monthly_rain_seattle.py
@@ -12,12 +12,17 @@ with open ('precipitation.json', encoding= 'utf8') as file:
 #step 2: Write a code to categorise the dates per month
 #limit data to only the first 
 months = []
+month_value = {}
 for observation in data:
     date = observation['date'].split('-')
-    month = [date[0], date[1]]
-    months.append(month)
-print(months)
-
+    observation['date'] = [date[0], date[1]]
+    #months.append(month)
+    # if month in data:
+    #     data[month] += int(data['value'])
+    # else:
+    #     data[month] - 
+#print(months)
+print(data)
 # for date in months:
     
 #     months.append(month)
