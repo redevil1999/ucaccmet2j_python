0c887f0e94a267c1c2d9e76fbdb45075ad078d8f
RobinDevillers <re.devillers1999@gmail.com>
2020-01-14 16:19:52 +0100

    Per station and made a json file

--- monthly_rain_seattle.py
+++ monthly_rain_seattle.py
@@ -12,24 +12,45 @@ with open ('precipitation.json', encoding= 'utf8') as file:
 #step 2: Write a code to categorise the dates per month
 #limit data to only the first 
 
-
-
-months = [0]*12
-month_value = {}
+months_seattle = [0]*12
 seattle = 'GHCND:US1WAKG0038'
 
 for observation in data:
     if observation['station'] == seattle:
             date = observation['date'].split('-')
             month = int(date[1]) -1
-            months[month] += observation['value']
+            months_seattle[month] += observation['value']
 
-print(months)
+months_cincinnati = [0]*12
+cincinnati = 'GHCND:USW00093814'
 
-# print(months)
-# print(f'In a year, the total amount of rain in Seattle is {sum(months)}')
+for observation in data:
+    if observation['station'] == cincinnati:
+            date = observation['date'].split('-')
+            month = int(date[1]) -1
+            months_cincinnati[month] += observation['value']
 
+months_maui = [0]*12
+maui = 'GHCND:USC00513317'
 
-#step 3: identify the values that are part of each month
+for observation in data:
+    if observation['station'] == maui:
+            date = observation['date'].split('-')
+            month = int(date[1]) -1
+            months_maui[month] += observation['value']
 
-#Step 4: create a total of the values
+months_sandiego = [0]*12
+sandiego = 'GHCND:US1CASD0032'
+
+for observation in data:
+    if observation['station'] == sandiego:
+            date = observation['date'].split('-')
+            month = int(date[1]) -1
+            months_sandiego[month] += observation['value']
+
+print(f'Seattle per month {months_seattle}')
+print(f'Cincinnati per month {months_cincinnati}')
+print(f'Maui per month {months_maui}')
+print(f'San Diego per month {months_sandiego}')
+print(f'In a year, the total amount of rain at these four stations is {sum(months_seattle) + sum(months_cincinnati) + sum(months_maui) + sum(months_sandiego)}')
+
