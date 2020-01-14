354e4250c404c7465f584a91b20536a195c4f6e0
RobinDevillers <re.devillers1999@gmail.com>
2020-01-14 16:55:40 +0100

    Added comments for clarification

--- monthly_rain_seattle.py
+++ monthly_rain_seattle.py
@@ -12,7 +12,9 @@ with open ('precipitation.json', encoding= 'utf8') as file:
 #step 2: Write a code to categorise the dates per month
 #limit data to only the first 
 
-
+#In the following, the code calculates the amount of rain per month per station. 
+#First, the station name is used to select only the station in Seattle
+#Second, it adds each value per month
 months_seattle = [0]*12
 seattle = 'GHCND:US1WAKG0038'
 
@@ -25,6 +27,8 @@ for observation in data:
 print(months_seattle)
 total_rain = sum(months_seattle)
 
+#The second assignment requested the percentage per month of the full year.
+#In the following, you can see the percentage calculations.
 
 percentage_per_month = []
 for month in months_seattle:
